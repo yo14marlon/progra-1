009b84980185aaf69bb3fb649e90f079904884bb
yo14marlon <123839415+yo14marlon@users.noreply.github.com>
2023-02-18 09:00:18 -0600

    Add files via upload

---
+++ Ejercicio/Contar_Letras.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ejercicio
+{
+    
+    public class Contar_Letras
+    {
+    
+        public int Letras(string letras)
+        {
+            int numeroLetras;
+            numeroLetras = letras.Length;
+            
+            return numeroLetras;
+        }
+    }
+}
---
+++ Ejercicio/Program.cs
@@ -0,0 +1,8 @@
+﻿using Ejercicio;
+Contar_Letras letras = new Contar_Letras();
+int LETRAS;
+
+Console.WriteLine("Ingresa tu texto");
+
+LETRAS = letras.Letras(Console.ReadLine());
+Console.WriteLine("Tu texto tiene " + LETRAS + " Letras");
